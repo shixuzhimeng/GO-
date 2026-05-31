d542c686c406c9ceb4bc7b6cd3fb464e7e6622a8
shixuzhimeng <1516446316@qq.com>
2026-05-31 22:13:32 +0800

    go 语言的基本语法的学习

--- start/fuzhi.go
+++ start/fuzhi.go
@@ -1,5 +1,7 @@
 package main
 
+import "fmt"
+
 // type Person struct {
 // 	name string
 // }
@@ -18,9 +20,44 @@ package main
 // }
 
 // 原组赋值
-func gcd(x, y int) int {
-	for y != 0 {
-		x, y = y, x%y
-	}
-	return x
+// func gcd(x, y int) int {
+// 	for y != 0 {
+// 		x, y = y, x%y
+// 	}
+// 	return x
+// }
+
+// 隐式的赋值行为
+// 可赋值性
+// func main() {
+// 	//medals := []string{"gold", "silver", "bronze"}
+// 	var medals [3]string
+// 	medals[0] = "gold"
+// 	medals[1] = "silver"
+// 	medals[2] = "bronze"
+
+// 	fmt.Println(medals)
+// }
+
+// 类型
+// type 类型名字 底层类型
+type Celsius float64
+
+// 摄氏温度
+type Fahrenheit float64 // 华氏温度
+const (
+	AbsoluteZeroC Celsius = -273.15 // 绝对零度
+	FreezingC     Celsius = 0       // 结冰点温度
+	BoilingC      Celsius = 100     // 沸水温度
+)
+
+func CToF(c Celsius) Fahrenheit { return Fahrenheit(c*9/5 + 32) }
+func FToC(f Fahrenheit) Celsius { return Celsius((f - 32) * 5 / 9) }
+
+func main() {
+	fmt.Printf("%g\n", BoilingC-FreezingC) // "100" °C
+	boilingF := CToF(BoilingC)
+	fmt.Printf("%g\n", boilingF-CToF(FreezingC)) // "180" °F
+	fmt.Printf("%g\n", boilingF-FreezingC)
+	// compile error: type mismatch
 }
