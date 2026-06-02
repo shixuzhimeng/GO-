1ed1e7cefb1d75254ec56526f7802177db7d3498
shixuzhimeng <1516446316@qq.com>
2026-06-03 00:11:29 +0800

    learn 数组相关的内容，slice和append

--- start/zuoyongyu.go
+++ start/zuoyongyu.go
@@ -1,7 +1,5 @@
 package main
 
-import "fmt"
-
 // func f() int {
 // 	return 5
 // }
@@ -37,27 +35,82 @@ import "fmt"
 //		fmt.Println() // 换行
 //	}
 
+//	func main() {
+//		x := "hello"
+//		for _, x := range x {
+//			x := x + 'A' - 'a'
+//			fmt.Printf("%c", x) // "HELLO" (one letter per iteration)
+//		}
+//	}
+
+// const (
+// 	deadbeef = 0xdeadbeef        // untyped int with value 3735928559
+// 	a        = uint32(deadbeef)  // uint32 with value 3735928559
+// 	b        = float32(deadbeef) // float32 with value 3735928576 (rounded up)
+// 	c        = float64(deadbeef) // float64 with value 3735928559 (exact)
+// 	d        = int32(deadbeef)   // compile error: constant overflows int32
+// 	e        = float64(1e309)    // compile error: constant overflows float64
+// 	f        = uint(-1)          // compile error: constant underflows uint
+// )
+
 // func main() {
-// 	x := "hello"
-// 	for _, x := range x {
-// 		x := x + 'A' - 'a'
-// 		fmt.Printf("%c", x) // "HELLO" (one letter per iteration)
+// 	var f float64 = 212
+// 	fmt.Println((f - 32) * 5 / 9)     // "100"; (f - 32) * 5 is a float64
+// 	fmt.Println(5 / 9 * (f - 32))     // "0";
+// 	fmt.Println(5.0 / 9.0 * (f - 32)) // "100"; 5.0/9.0 is an untyped float
+// }
+
+//	func main() {
+//		i := 0
+//		r := '\000'
+//		f := 0.0
+//		fmt.Println(i)
+//		fmt.Println(r)
+//		fmt.Println(f)
+//	}
+//
+
+// 内置的append函数用于向slice追加元素
+// func appendInt(x []int, y int) []int {
+// 	var z []int
+// 	zlen := len(x) + 1
+// 	if zlen <= cap(x) {
+// 		z = x[:zlen]
+// 	} else {
+
+// 		zcap := zlen
+// 		if zcap < 2*len(x) {
+// 			zcap = 2 * len(x)
+// 		}
+// 		z = make([]int, zlen, zcap)
+// 		copy(z, x)
+// 	}
+// 	z[len(x)] = y
+// 	return z
+// }
+
+// func main() {
+// 	var x, y []int
+// 	for i := 0; i < 10; i++ {
+// 		y = appendInt(x, i)
+// 		fmt.Printf("%d cap=%d\t%v\n", i, cap(y), y)
+// 		x = y
 // 	}
 // }
 
-func main() {
-	var f float64 = 212
-	fmt.Println((f - 32) * 5 / 9)     // "100"; (f - 32) * 5 is a float64
-	fmt.Println(5 / 9 * (f - 32))     // "0";
-	fmt.Println(5.0 / 9.0 * (f - 32)) // "100"; 5.0/9.0 is an untyped float
+func appendInt(x []int, y ...int) []int {
+	var z []int
+	copy(z[len(x):], y)
+	return z
 }
 
-const (
-	deadbeef = 0xdeadbeef        // untyped int with value 3735928559
-	a        = uint32(deadbeef)  // uint32 with value 3735928559
-	b        = float32(deadbeef) // float32 with value 3735928576 (rounded up)
-	c        = float64(deadbeef) // float64 with value 3735928559 (exact)
-	d        = int32(deadbeef)   // compile error: constant overflows int32
-	e        = float64(1e309)    // compile error: constant overflows float64
-	f        = uint(-1)          // compile error: constant underflows uint
-)
+func nonempty(strings []string) []string {
+	i := 0
+	for _, s := range strings {
+		if s != "" {
+			strings[i] = s
+			i++
+		}
+	}
+	return strings[:i]
+}
